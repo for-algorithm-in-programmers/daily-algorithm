538aa68724b210c336c8c68dcd2ba21863b65d4e
Ahhhhhyeong <dkdud081@gmail.com>
2025-05-15 16:59:41 +0900

    0515 의상 2차시도 성공

--- Day04/Ahyeong0515.java
+++ Day04/Ahyeong0515.java
@@ -14,15 +14,16 @@ public class Ahyeong0515 {
 
 class Solution {
   public int solution(String[][] clothes) {
-      int answer = clothes.length;
+      int answer = 1;
       // 옷 종류별로 map을 만듦
       Map<String, List<String>> map = new HashMap<>();
       for (String[] strings : clothes) {
           map.computeIfAbsent(strings[1], k -> new ArrayList<>()).add(strings[0]);
       }
       for (String key : map.keySet()) {
-        answer *= map.get(key).size();
+        answer *= (map.get(key).size()+1);
       }
+      // 안입는 경우 빼기
       return answer-1;
   }
 }
