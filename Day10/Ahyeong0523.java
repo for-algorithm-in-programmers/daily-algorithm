6ad3600cbd7a3bc8a78f4a5764ab0b9e5d4e5bfe
Ahhhhhyeong <dkdud081@gmail.com>
2025-05-24 19:28:49 +0900

    소수찾기: 중복체크 부분 제거한 코드

--- Day10/Ahyeong0523.java
+++ Day10/Ahyeong0523.java
@@ -5,50 +5,51 @@ import java.util.*;
 public class Ahyeong0523 {
 
   public static void main(String[] args) {
-    
-    //String numbers = "17"; //3 
-    //String numbers = "011"; //2
-    //String numbers = "123"; 
-    String numbers = "013"; //4
-     
+
+    // String numbers = "17"; //3
+    // String numbers = "011"; //2
+    // String numbers = "123";
+    String numbers = "013"; // 4
+
     Solution solution = new Solution();
     System.out.println(solution.solution(numbers));
   }
 }
 
 class Solution {
-  Set<Integer> nSet = new HashSet<>(); //Set을 이용해 중복제거  
-  Set<String> visited = new HashSet<>(); // 반복값 체크
+  Set<Integer> nSet = new HashSet<>(); // Set을 이용해 중복제거
+
   public int solution(String numbers) {
-      // 조합
-      getCombinations("", numbers); 
-      return nSet.size();
+    // 조합
+    getCombinations("", numbers);
+    return nSet.size();
   }
-  //소수판별
-  public boolean isPrime(int n){
-    if(n < 2) return false;
-    for(int i = 2; i<=(int)Math.sqrt(n); i++){
-      if(n%i == 0) return false;
+
+  // 소수판별
+  public boolean isPrime(int n) {
+    if (n < 2)
+      return false;
+    for (int i = 2; i <= (int) Math.sqrt(n); i++) {
+      if (n % i == 0)
+        return false;
     }
     return true;
   }
+
   // 만들 수 있는 모든 숫자 만들기
   public void getCombinations(String comb, String others) {
     if (!comb.equals("")) {
-        int num = Integer.parseInt(comb);
-        if (!visited.contains(comb)) {
-            visited.add(comb);
-            if (isPrime(num)) {
-                nSet.add(num);
-            }
-        }
+      int num = Integer.parseInt(comb);
+      if (isPrime(num)) {
+        nSet.add(num);
+      }
+
     }
 
     for (int i = 0; i < others.length(); i++) {
-        getCombinations(comb + others.charAt(i), 
-                        others.substring(0, i) + others.substring(i + 1));
+      getCombinations(comb + others.charAt(i),
+          others.substring(0, i) + others.substring(i + 1));
     }
   }
 
-  
 }
